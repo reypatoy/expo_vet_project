391974e8b8c71a9bebfea35ad95eae4149e8c429
reypatoy <85288525+reypatoy@users.noreply.github.com>
2021-10-03 22:44:55 +0800

    Initial for app drawer.

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Home from './components/screens/home';
+import Services from './components/screens/services';
+
+
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <NavigationContainer>
+     <Drawer.Navigator initialRouteName="Home">
+       <Drawer.Screen name="Home" component={Home} />
+       <Drawer.Screen name="Services" component={Services} />
+     </Drawer.Navigator>
+  </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  }
